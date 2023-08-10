afe2e4d59950a4aa5950c4649f6dd28e4aded713
HarryDerbyshire <61466338+HarryDerbyshire@users.noreply.github.com>
2023-08-10 21:23:23 +0100

    cmd\suggest: Add new EmbedBuilder for message body

--- commands/player/suggest.js
+++ commands/player/suggest.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 const { Suggestions } = require('../../utils/database.js');
 
 module.exports = {
@@ -23,5 +24,50 @@ module.exports = {
 		});
 		interaction.reply({ content: `Suggestion has been created. \nDescription: ${suggestionDescription}\nReceive Updates: ${suggestionUpdates}`, ephemeral: true });
 		suggestionChannel.send(lastRecord.description);
+		try {
+			const lastRecord = await Suggestions.findOne({
+				order: [['createdAt', 'DESC']],
+			});
+
+			let integerValue = parseInt(lastRecord.id, 10);
+			integerValue += 1;
+			integerValue.toString();
+			integerValue = (`000${integerValue}`).slice(-4);
+
+			const suggestionEmbed = new EmbedBuilder()
+				.setColor('#1f1f1f')
+				.setTitle(`Suggestion #${integerValue}`)
+				.setDescription(`${suggestionDescription}`)
+				.addFields({ name: 'State', value: 'New', inline: true });
+
+			try {
+				suggestionChannel.send({ embeds: [suggestionEmbed] }).then(async (sent) => {
+					const embedId = sent.id;
+					await Suggestions.create({
+						id: integerValue,
+						description: suggestionDescription,
+						username: interaction.user.id,
+						status: 0,
+						embedMessageId: embedId,
+					});
+
+					sent.startThread({
+						name: `Suggestion #${integerValue} - Discussion`,
+						autoArchiveDuration: 1440,
+					});
+
+					sent.react(interaction.guild.emojis.cache.get('888238461541302373'))
+						.then(() => sent.react(interaction.guild.emojis.cache.get('888238495280287774')));
+
+					suggestionLogChannel.send(`**${interaction.user.username}** has created suggestion **#${integerValue}**`);
+				});
+			}
+			catch (e) {
+				console.log(e);
+			}
+		}
+		catch (e) {
+			console.log(e);
+		}
 	},
 };
