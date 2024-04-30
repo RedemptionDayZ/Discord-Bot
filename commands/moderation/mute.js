864413b42ebb8dadd3e50e0df421a6d2c9346189
HarryDerbyshire <61466338+HarryDerbyshire@users.noreply.github.com>
2024-04-30 01:25:47 +0100

    cmd\mute: Add mute reason option

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -10,10 +10,15 @@ module.exports = {
 				.setName('member')
 				.setDescription('The member you wish to mute.')
 				.setRequired(true)),
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('reason')
+				.setDescription('Message to be displayed in chat alongside mute. If empty mute is not announced.')),
 	category: 'moderation',
 	permissionLevel: 1,
 	async execute(interaction) {
 		const mutedMember = interaction.options.getUser('member', true);
+		const muteReason = interaction.options.getString('reason');
 
 		
 		interaction.reply({ content: 'The ID must be 4 characters', ephemeral: true });
