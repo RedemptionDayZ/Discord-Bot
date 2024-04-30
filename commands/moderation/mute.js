2c2a3ea9b9a66f95e39c3e90bb2a71fcf62b2ca3
HarryDerbyshire <61466338+HarryDerbyshire@users.noreply.github.com>
2024-04-30 01:26:52 +0100

    cmd\mute: Add embed and reply formats

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 const config = require('../../config.json');
 
 module.exports = {
@@ -9,7 +9,6 @@ module.exports = {
 			option
 				.setName('member')
 				.setDescription('The member you wish to mute.')
-				.setRequired(true)),
 				.setRequired(true))
 		.addStringOption(option =>
 			option.setName('reason')
@@ -17,10 +16,27 @@ module.exports = {
 	category: 'moderation',
 	permissionLevel: 1,
 	async execute(interaction) {
-		const mutedMember = interaction.options.getUser('member', true);
+		const mutedMember = interaction.options.getMember('member', true);
 		const muteReason = interaction.options.getString('reason');
 
-		
-		interaction.reply({ content: 'The ID must be 4 characters', ephemeral: true });
+		const muteEmbed = new EmbedBuilder()
+			.setColor('#fc4242')
+			.setTitle(`Member <@!${mutedMember.id}> muted`)
+			.addFields({ name: 'Mute Reason', value: muteReason, inline: true });
+
+		if (mutedMember.roles.cache.has(config.muteRoleID)) {
+			interaction.reply({ content: `<@!${mutedMember.id}> is already muted, use \`\\Unmute <member>\` to unmute them.`, ephemeral: true });
+			return;
+		}
+
+		mutedMember.roles.add(config.muteRoleID);
+
+		if (muteReason) {
+			interaction.reply({ embeds: [muteEmbed] });
+		}
+		else {
+			interaction.reply({ content: `<@!${mutedMember.id}> has been muted`, ephemeral: true });
+		}
+		return;
 	},
 };
