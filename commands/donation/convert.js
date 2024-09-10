ab2282cc0adc62ad1faa5f28b16cab310643ee00
HarryDerbyshire <61466338+HarryDerbyshire@users.noreply.github.com>
2024-09-10 19:42:48 +0100

    cmd\convert: Initial commit

---
+++ commands/donation/convert.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('currency-convert')
+		.setDescription('Converts currencies to GBP.')
+		.addNumberOption(option =>
+			option.setName('amount')
+				.setDescription('The donation amount in GBP.')
+				.setRequired(true)
+				.setMinValue(0.01))
+		.addNumberOption(option =>
+			option.setName('currency')
+				.setDescription('Currency code you are converting from.')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'USD - United States Dollar', value: 'USD' },
+					{ name: 'EUR - Euros', value: 'EUR' },
+				)),
+	category: 'donation',
+	async execute(interaction) {
+		const conversionAmount = interaction.options.getNumber('amount', true);
+		const conversionCurrency = interaction.options.getString('group', true);
+	},
+};
