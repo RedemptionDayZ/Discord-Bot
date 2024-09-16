8267d05587d098d71cf4f2bf511ae46db7b9549b
HarryDerbyshire <61466338+HarryDerbyshire@users.noreply.github.com>
2024-09-16 22:36:10 +0100

    cmd\suggest: Add validation and messages for DM

--- commands/suggestions/suggest.js
+++ commands/suggestions/suggest.js
@@ -18,7 +18,24 @@ module.exports = {
 		const suggestionChannel = interaction.client.channels.cache.get(config.suggestionChannel);
 		const suggestionLogChannel = interaction.client.channels.cache.get(config.logChannel);
 		const suggestionDescription = interaction.options.getString('suggestion', true).toLowerCase();
-		const suggestionUpdates = interaction.options.getBoolean('get-updates') ?? false;
+		let suggestionUpdates = interaction.options.getBoolean('get-updates') ?? false;
+		let dmUpdateMessage = '';
+
+		if (suggestionUpdates) {
+			const dmNotificationEmbed = new EmbedBuilder()
+				.setColor(config.status[0][1])
+				.setTitle('Thank you for creating a suggestion')
+				.setDescription('_You opted in to updates on Redemption for this particular suggestion._');
+			await interaction.user.send({ embeds: [dmNotificationEmbed] })
+				.catch(error => {
+					if (error.code !== 50007) console.log(error);
+					suggestionUpdates = false;
+					dmUpdateMessage = '\n\n**You opted in to receive updates but you do not have DMs enabled so you will not receive them.**';
+				});
+		}
+		else {
+			dmUpdateMessage = '\n\nYou did not opt in for updates on this suggestion.';
+		}
 
 		try {
 			const lastRecord = await Suggestions.findOne({
