b219a6cd522b10e26a3424f443e2763ff69708f0
HarryDerbyshire <61466338+HarryDerbyshire@users.noreply.github.com>
2024-09-17 17:31:34 +0100

    cmd\convert: Add ability to reverse conversion

--- commands/donation/convert.js
+++ commands/donation/convert.js
@@ -4,26 +4,37 @@ const { currencyApiKey } = require('../../credentials.json');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('currency-convert')
-		.setDescription('Converts currencies to GBP.')
+		.setDescription('Convert between currencies.')
 		.addNumberOption(option =>
 			option.setName('amount')
 				.setDescription('The original amount you wish to convert.')
 				.setRequired(true)
 				.setMinValue(0.01))
 		.addStringOption(option =>
-			option.setName('currency')
+			option.setName('currency-from')
 				.setDescription('Currency code you are converting from.')
 				.setRequired(true)
 				.addChoices(
 					{ name: 'USD - United States Dollar', value: 'USD' },
 					{ name: 'EUR - Euros', value: 'EUR' },
+					{ name: 'GBP - Great British Pound', value: 'GBP' },
+				))
+		.addStringOption(option =>
+			option.setName('currency-to')
+				.setDescription('Currency code you are converting from.')
+				.setRequired(false)
+				.addChoices(
+					{ name: 'USD - United States Dollar', value: 'USD' },
+					{ name: 'EUR - Euros', value: 'EUR' },
+					{ name: 'GBP - Great British Pound', value: 'GBP' },
 				)),
 	category: 'donation',
 	async execute(interaction) {
 		const conversionAmount = interaction.options.getNumber('amount', true);
-		const conversionCurrency = interaction.options.getString('currency', true);
+		const conversionCurrencyFrom = interaction.options.getString('currency-from', true);
+		const conversionCurrencyTo = interaction.options.getString('currency-to') ?? 'GBP';
 
-		const apiUrl = `https://api.currencybeacon.com/v1/convert?api_key=${currencyApiKey}&from=${conversionCurrency}&to=GBP&amount=${conversionAmount}`;
+		const apiUrl = `https://api.currencybeacon.com/v1/convert?api_key=${currencyApiKey}&from=${conversionCurrencyFrom}&to=${conversionCurrencyTo}&amount=${conversionAmount}`;
 
 		fetch(apiUrl)
 			.then(response => {
@@ -33,7 +44,7 @@ module.exports = {
 				return response.json();
 			})
 			.then(data => {
-				interaction.reply(`${conversionAmount}${conversionCurrency} = ${data.response.value.toFixed(2)}GBP`);
+				interaction.reply(`${conversionAmount}${conversionCurrencyFrom} = ${data.response.value.toFixed(2)}${conversionCurrencyTo}`);
 			})
 			.catch(error => {
 				console.error('Currency API:', error);
