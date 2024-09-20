92d888aa97953c998c801999b38b70ed2c7eab1e
HarryDerbyshire <61466338+HarryDerbyshire@users.noreply.github.com>
2024-09-20 18:59:40 +0100

    evt\msgCreate: Add DM and fallback logic

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,8 +1,23 @@
 const { Events } = require('discord.js');
+const config = require('../config.json');
 
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
 		if (message.author.bot) return;
+		if (message.channelId == config.suggestionChannel) {
+			const messageText = 'If you want to create a suggestion use the command `/suggest` and then use provided threads for discussion.';
+
+			message.member.send(messageText)
+				.catch(async error => {
+					if (error.code === 50007) {
+						console.log(error);
+						const messageReply = await message.channel.send(messageText);
+						setTimeout(() => messageReply.delete(), 10000);
+					}
+					else { console.log(error); }
+				})
+				.then(message.delete());
+		}
 	},
 };
