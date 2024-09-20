7a3790e6c93778ad4e9570d11c313054d5e9aa6d
HarryDerbyshire <61466338+HarryDerbyshire@users.noreply.github.com>
2024-09-20 18:59:11 +0100

    evt\msgCreate: Add bot exclusion

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,6 +3,6 @@ const { Events } = require('discord.js');
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
-		console.log(message);
+		if (message.author.bot) return;
 	},
 };
