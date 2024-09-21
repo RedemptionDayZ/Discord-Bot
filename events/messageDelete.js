ef7bce980ef50a7e7d54edf3c85e4f022115fa45
HarryDerbyshire <61466338+HarryDerbyshire@users.noreply.github.com>
2024-09-21 01:08:01 +0100

    evt\msgDelete: Add archive logic on suggestion delete

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,11 +1,29 @@
 const { Events } = require('discord.js');
 const config = require('../config.json');
+const { Suggestions } = require('../utils/database.js');
 
 module.exports = {
 	name: Events.MessageDelete,
 	async execute(message) {
 		if (message.channelId == config.suggestionChannel) {
-
+			let suggestions;
+			try {
+				suggestions = await Suggestions.findOne({
+					where: {
+						embedMessageId: message.id,
+					},
+				});
+				if (suggestions) {
+					await Suggestions.update({ archived: true }, {
+						where: {
+							embedMessageId: message.id,
+						},
+					});
+				}
+			}
+			catch (e) {
+				console.log(e);
+			}
 		}
 	},
 };
